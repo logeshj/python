8c266d41136c6dad370091287ae8e2ea51c43de6
logeshj <42931494+logeshj@users.noreply.github.com>
2018-09-20 10:55:13 +0530

    Update python.py

--- python.py
+++ python.py
@@ -23,3 +23,15 @@ print now.strftime("%Y-%m-%d %H:%M")
 print
 print "Current date and time using isoformat:"
 print now.isoformat()
+
+
+# Python Program - Get IP Address
+
+import socket;
+print("Want to get IP Address ? (y/n): ");
+check = input();
+if check == 'n':
+    exit();
+else:
+    print("\nYour IP Address is: ",end="");
+    print(socket.gethostbyname(socket.gethostname()));
